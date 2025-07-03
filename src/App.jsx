9a619ff65b359011d153773512254f0d8543d092
ALMAZ-09021 <almaz.khaybullin@mail.ru>
2025-07-03 15:56:13 +0300

    Added card components and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {useState} from "react";
+
+function Card ({title}) {
+    const [hasLiked, setHasLiked] = useState(false)
+
+    return (
+        <div className="card">
+            <h2>{title}</h2>
+        <button onClick={() => setHasLiked(!hasLiked)}>
+            {hasLiked ? 'Liked' :'Like'}
+        </button>
+        </div>
+    )
+}
+
 
 function App() {
 
     return (
-        <>
-
-        </>
+        <div className="card-container">
+            <h2>Functional card component</h2>
+            <Card title='Lion King'/>
+            <Card title='Avatar'/>
+        </div>
     )
 }
+
 export default App
