ae1e0016d966741402ab4b32e55d0e60b5633995
ALMAZ-09021 <almaz.khaybullin@mail.ru>
2025-07-04 12:53:51 +0300

    Added optimize search movie function

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import React, {useState, useEffect} from 'react'
 import Search from "./components/Search.jsx";
 import Spinner from "./components/Spinner.jsx";
 import MovieCard from "./components/MovieCard.jsx";
+import {useDebounce} from 'react-use'
+
 
 const API_BASE_URL='https://api.themoviedb.org/3'
-
 const API_KEY=import.meta.env.VITE_TMDB_API_KEY
 
 const API_OPTIONS ={
@@ -19,19 +20,21 @@ function App() {
 
     const [searchTerm, setSearchTerm] = useState('')
     const [errorMessage, setErrorMessage] = useState('')
-
     const [movieList, setMovieList] = useState([])
     const [isLoading, setIsLoading] = useState(false)
+    const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('')
 
+     useDebounce(() => setDebouncedSearchTerm(searchTerm), 500,
+         [searchTerm])
 
-    const fetchMovies = async() => {
+    const fetchMovies = async(query='') => {
         setIsLoading(true)
         setErrorMessage('')
 
-
-
         try {
-        const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
+        const endpoint = query
+            ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+            : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
         const response = await fetch(endpoint, API_OPTIONS)
 
             if(!response.ok){
@@ -55,8 +58,8 @@ function App() {
 }
 
     useEffect(()=>{
-        fetchMovies()
-    },[])
+        fetchMovies(debouncedSearchTerm)
+    },[debouncedSearchTerm])
 
     return (
         <main>
